cc1925b398b0626490e32fd4b84ee23738d80f43
Joshua Charles Campbell <gitcommit@orezpraw.com>
2014-02-11 11:48:49 -0700

    re-add midstate field not sure its right, but it replicates upstream behavior

--- p2pool/bitcoin/getwork.py
+++ p2pool/bitcoin/getwork.py
@@ -51,7 +51,7 @@ class BlockAttempt(object):
             'data': _swap4(block_data).encode('hex') + '000000800000000000000000000000000000000000000000000000000000000000000000000000000000000080020000',
             #'hash1': '00000000000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000010000',
             'target': pack.IntType(256).pack(self.share_target).encode('hex'),
-            #'midstate': _swap4(midhash.state).encode('hex'),
+            'midstate': _swap4(midhash.state).encode('hex'),
         }
         
         getwork = dict(getwork)
